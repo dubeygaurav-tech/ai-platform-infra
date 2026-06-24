dd848441d968ed6209ffd2c767703f021a1f026d
Gaurav Dubey <dubeygaurav.tech@gmail.com>
2026-06-24 15:21:17 +0200

    added KEDA for auto scaling

--- BOOTSTRAP-SCRIPT.sh
+++ BOOTSTRAP-SCRIPT.sh
@@ -12,9 +12,17 @@ kubectl apply --server-side -n argocd -f https://raw.githubusercontent.com/argop
 helm repo add external-secrets https://charts.external-secrets.io
 helm install external-secrets external-secrets/external-secrets -n external-secrets
 
+# Add KEDA repo
+helm repo add kedacore https://kedacore.github.io/charts
+helm repo update
+
+# Install KEDA
+helm install keda kedacore/keda --namespace keda --create-namespace
+
 # 4. Install Vault (Dev Mode)
 helm repo add hashicorp https://helm.releases.hashicorp.com
 helm install vault hashicorp/vault --namespace vault --set "server.dev.enabled=true"
+
 
 # 5. Provide the "Root of Trust" secret (for ESO to talk to Vault)
 kubectl create secret generic vault-token -n external-secrets --from-literal=token=$ROOT_TOKEN
